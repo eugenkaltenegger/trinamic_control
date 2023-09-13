3ca382b20c7044cdeb8d69727ac6071cac6ce5a9
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-09-13 12:43:09 +0200

    trinamic_parameter implementation

--- src/trinamic_control/trinamic_parameter/trinamic_parameter.cpp
+++ src/trinamic_control/trinamic_parameter/trinamic_parameter.cpp
@@ -6,5 +6,30 @@ using trinamic_control::TrinamicParameter;
 
 TrinamicParameter::TrinamicParameter(YAML::Node yaml)
 {
-  return;
+  if (yaml["address"].IsDefined() && !yaml["address"].IsNull()) {
+    this->address_ = std::make_unique<int>(yaml["address"].as<int>());
+  }
+
+  if (yaml["identifier"].IsDefined() && !yaml["identifier"].IsNull()) {
+    this->identifier_ = std::make_unique<std::string>(yaml["identifier"].as<std::string>());
+  }
+
+  if (yaml["description"].IsDefined() && !yaml["description"].IsNull()) {
+    this->description_ = std::make_unique<std::string>(yaml["description"].as<std::string>());
+  }
+}
+
+int TrinamicParameter::getAddress()
+{
+  return *this->address_;
+}
+
+std::string TrinamicParameter::getIdentifier()
+{
+  return *this->identifier_;
+}
+
+std::string TrinamicParameter::getDescription()
+{
+  return *this->description_;
 }
